c4a6b855fd906835698eaaf79c5c5c864ab7117f
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-29 18:56:03 -0400

    init xcode proj

---
+++ Raffles/Raffles/RafflesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafflesApp.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct RafflesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
