a3dadeb13652540fe8052a9518b995541b16b991
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 09:05:50 -0400

    Implemented request method.  Implemented placeholders for get and post methods.

--- Raffles/Raffles/Networking/RaffleAPIClient.swift
+++ Raffles/Raffles/Networking/RaffleAPIClient.swift
@@ -10,7 +10,7 @@ import Combine
 
 #warning("TODO: REMOVE PLACEHOLDERS AND IMPLEMENT THE PROTOCOL STUBS.")
 final class RaffleAPIClient {
-    static let baseURL = URL(staticString: "https://raffle-fs-app.herokuapp.com")
+    private let baseURL = URL(staticString: "https://raffle-fs-app.herokuapp.com")
     private let session: URLSession
     private let decoder: JSONDecoder
     
@@ -20,22 +20,36 @@ final class RaffleAPIClient {
     }
     
     #warning("TODO: Add Path Components")
-    static func makeURL() -> URL {
+    func makeURL() -> URL {
         return baseURL
     }
 }
 
-////MARK: API Methods
-//extension RaffleAPIClient: API {
-//    func request<T>(_ request: URLRequest) -> AnyPublisher<T, APIError> where T : Decodable {
-//      
-//    }
-//    
-//    func get<T>() -> AnyPublisher<T, APIError> where T : Decodable {
-//        <#code#>
-//    }
-//    
-//    func post<T>() -> AnyPublisher<T, APIError> where T : Decodable {
-//        <#code#>
-//    }
-//}
+//MARK: API Methods
+extension RaffleAPIClient: API {
+    func request<T>(_ request: URLRequest) -> AnyPublisher<T, APIError> where T : Decodable {
+        session
+            .dataTaskPublisher(for: request)
+            .retry(1)
+            .mapError { _ in .noInternet }
+            .flatMap { data, response -> AnyPublisher<T, APIError> in
+                return Fail(error: APIError.unknown)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    #warning("TODO: Add GET")
+    func get<T>() -> AnyPublisher<T, APIError> where T : Decodable {
+        let urlRequest = URLRequest(url: makeURL())
+        return request(urlRequest)
+            .eraseToAnyPublisher()
+    }
+
+    #warning("TODO: Add POST")
+    func post<T>() -> AnyPublisher<T, APIError> where T : Decodable {
+        let urlRequest = URLRequest(url: makeURL())
+        return request(urlRequest)
+            .eraseToAnyPublisher()
+    }
+}
