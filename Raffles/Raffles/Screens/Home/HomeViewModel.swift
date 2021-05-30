11402f34759795c3d15b5d7573b47e5e2b40cd9b
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 14:45:11 -0400

    RafflesViewModel methods moved to HomeViewModel.  Deleted RafflesViewModel.

--- Raffles/Raffles/Screens/Home/HomeViewModel.swift
+++ Raffles/Raffles/Screens/Home/HomeViewModel.swift
@@ -9,9 +9,12 @@ import Foundation
 import Combine
 
 final class HomeViewModel: ObservableObject {
+    //MARK: - Properties
     @Published var raffleName: String
     @Published var secretToken: String
     @Published var allRaffles: [Raffle]
+    
+    private var cancellable: AnyCancellable?
     
     init(raffleName: String = "",
          secretToken: String = "",
@@ -20,5 +23,35 @@ final class HomeViewModel: ObservableObject {
         self.raffleName = raffleName
         self.secretToken = secretToken
         self.allRaffles = raffles
+    }
+}
+
+//Mark: Network Calls
+extension HomeViewModel {
+    typealias RafflesPublisher = AnyPublisher<[Raffle],APIError>
+
+    func createRaffle() {
+        
+    }
+    
+    func getRaffles()  {
+        cancellable = rafflesPublisher()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    print(error)
+                }
+            }, receiveValue: { [weak self] receivedRaffles in
+                #if DEBUG
+                dump(receivedRaffles)
+                #endif
+                self?.allRaffles = receivedRaffles
+            })
+    }
+    
+    private func rafflesPublisher() -> RafflesPublisher {
+        RaffleAPIClient
+            .shared
+            .get(endpoint: .allRaffles)
     }
 }
