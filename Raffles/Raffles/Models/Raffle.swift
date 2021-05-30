22535ee18f7a8d5b2cc18d8ed7b8b76d81508f94
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 08:48:37 -0400

    Created Raffle Decodable Struct

---
+++ Raffles/Raffles/Models/Raffle.swift
@@ -0,0 +1,24 @@
+//
+//  Raffle.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/30/21.
+//
+
+import Foundation
+
+struct Raffle: Decodable {
+  let id: Int
+  let name: String
+  let createdAt: Date
+  let raffledAt: Date?
+  let winnerId: Int?
+
+  private enum CodingKeys: String, CodingKey {
+    case id
+    case name
+    case createdAt = "created_at"
+    case raffledAt = "raffled_at"
+    case winnerId = "winner_id"
+  }
+}
--- Raffles/Raffles/Networking/RaffleAPIClient.swift
+++ Raffles/Raffles/Networking/RaffleAPIClient.swift
@@ -9,15 +9,25 @@ import Foundation
 import Combine
 
 #warning("TODO: REMOVE PLACEHOLDERS AND IMPLEMENT THE PROTOCOL STUBS.")
-struct RaffleAPIClient {
+final class RaffleAPIClient {
+    static let baseURL = URL(staticString: "https://raffle-fs-app.herokuapp.com")
+    private let session: URLSession
+    private let decoder: JSONDecoder
     
- 
+    init(session: URLSession = .shared, decoder: JSONDecoder = .init()) {
+        self.session = session
+        self.decoder = decoder
+    }
+    
+    #warning("TODO: Add Path Components")
+    static func makeURL() -> URL {
+        return baseURL
+    }
 }
 
 //extension RaffleAPIClient: API {
 //    //MARK: API Methods
 //    func request<T>(_ request: URLRequest) -> AnyPublisher<T, Error> where T : Decodable {
-//
 //    }
 //
 //    func get<T>() -> AnyPublisher<T, Error> where T : Decodable {
