e4b39e5fe3b23ada16804de07e92e1dc17e71de7
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 19:23:26 -0400

    created ParticipantsViewModel on a new file.

---
+++ Raffles/Raffles/Screens/RaffleParticipantsView/ParticipantsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ParticipantsViewModel.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/30/21.
+//
+
+import Foundation
+import Combine
+
+final class ParticipantsViewModel: ObservableObject {
+    init() {}
+}
