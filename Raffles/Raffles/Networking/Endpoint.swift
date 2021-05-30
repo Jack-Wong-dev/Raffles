1e2ed33b60d845ca36b1eb8e2d6213193dced526
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 10:13:45 -0400

    Added method to Endpoint enum.  It returns the path required for network calls.

--- Raffles/Raffles/Networking/Endpoint.swift
+++ Raffles/Raffles/Networking/Endpoint.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
-enum Endpoint: String {
-    case raffle = "raffles"
+enum Endpoint {
+    case allRaffles
+    case createRaffle
+    case retrieve(id: Int)
+    case participants(id: Int)
+    case signUp(id: Int)
+    case pickWinner(id: Int)
+    case retrieveWinner(id: Int)
+    
+    func path() -> String {
+        switch self {
+        case .allRaffles, .createRaffle:
+            return "api/raffles"
+        case .retrieve(id: let id):
+            return "api/raffles/\(id)"
+        case .participants(id: let id), .signUp(id: let id):
+            return "api/raffles/\(id)/participants"
+        case .pickWinner(id: let id), .retrieveWinner(id: let id):
+            return "api/raffles/\(id)/winner"
+        }
+    }
 }
