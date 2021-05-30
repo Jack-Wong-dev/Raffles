e92985ffa5fc57bfdd286a78fc4373948f425b3f
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 14:14:52 -0400

    Created PickAWinnerView file.

---
+++ Raffles/Raffles/Screens/PickAWinnerView.swift
@@ -0,0 +1,20 @@
+//
+//  PickAWinnerView.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/30/21.
+//
+
+import SwiftUI
+
+struct PickAWinnerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickAWinnerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickAWinnerView()
+    }
+}
