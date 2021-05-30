fda1f560bd91d7b566f2258d155285374fcae82a
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 08:17:14 -0400

    Added extension to URL, including an initializer to use a 'StaticString' to avoid having to force unwrap.

--- Raffles/Raffles/Networking/RaffleAPIClient.swift
+++ Raffles/Raffles/Networking/RaffleAPIClient.swift
@@ -17,14 +17,14 @@ struct RaffleAPIClient {
 //extension RaffleAPIClient: API {
 //    //MARK: API Methods
 //    func request<T>(_ request: URLRequest) -> AnyPublisher<T, Error> where T : Decodable {
-//        
+//
 //    }
-//    
+//
 //    func get<T>() -> AnyPublisher<T, Error> where T : Decodable {
-//        
+//
 //    }
-//    
+//
 //    func post<T>() -> AnyPublisher<T, Error> where T : Decodable {
-//        
+//
 //    }
 //}
