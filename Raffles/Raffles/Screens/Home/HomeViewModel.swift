a8dbddb51c3dbf0ee78bbb5d2449cd096c1f8fb6
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 14:36:15 -0400

    Created HomeViewModel and it's initializer.

--- Raffles/Raffles/Screens/Home/HomeViewModel.swift
+++ Raffles/Raffles/Screens/Home/HomeViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import Combine
+
+final class HomeViewModel: ObservableObject {
+    @Published var raffleName: String
+    @Published var secretToken: String
+    @Published var allRaffles: [Raffle]
+    
+    init(raffleName: String = "",
+         secretToken: String = "",
+         raffles: [Raffle] = []
+    ) {
+        self.raffleName = raffleName
+        self.secretToken = secretToken
+        self.allRaffles = raffles
+    }
+}
