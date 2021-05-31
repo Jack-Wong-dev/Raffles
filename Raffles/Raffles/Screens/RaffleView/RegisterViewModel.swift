be9d610d517e73971954ac3434519ad1d310a554
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 21:10:56 -0400

    Implemented RegisterViweModel init to support Dependency Injection.

--- Raffles/Raffles/Screens/RaffleView/RegisterViewModel.swift
+++ Raffles/Raffles/Screens/RaffleView/RegisterViewModel.swift
@@ -9,5 +9,27 @@ import Foundation
 import Combine
 
 final class RegisterViewModel: ObservableObject {
-    init() {}
+    @Published var firstName: String
+    @Published var lastName: String
+    @Published var email: String
+    @Published var phoneNumber: String
+    
+    init(
+        firstName: String = "",
+        lastName: String = "",
+        email: String = "",
+        phoneNumber: String = ""
+    ) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.phoneNumber = phoneNumber
+    }
+    
+    func reset() {
+        firstName = .init()
+        lastName = .init()
+        email = .init()
+        phoneNumber = .init()
+    }
 }
