f7b7366200b2376a96e7cae4fa16899f0751fce3
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-31 14:52:08 -0400

    Created static methods to return validation publishers.

---
+++ Raffles/Raffles/Utilities/Validation/Published-Extension .swift
@@ -0,0 +1,18 @@
+//
+//  Published-Extension .swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/31/21.
+//
+
+import Foundation
+import Combine
+
+extension Published.Publisher where Value == String {
+    typealias ValidationErrorClosure = () -> String
+    typealias ValidationPublisher = AnyPublisher<Validation, Never>
+    
+    func nonEmptyValidator(_ errorMessage: @autoclosure @escaping ValidationErrorClosure, dropFirst: Bool = true) -> ValidationPublisher {
+        return ValidationPublishers.nonEmptyValidation(for: self, errorMessage: errorMessage(), dropFirst: dropFirst)
+    }
+}
--- Raffles/Raffles/Utilities/Validation/ValidationPublishers.swift
+++ Raffles/Raffles/Utilities/Validation/ValidationPublishers.swift
@@ -8,8 +8,25 @@
 import Foundation
 import Combine
 
-final class ValidationPublishers {
-    init() {
+class ValidationPublishers {
+    typealias ValidationErrorClosure = () -> String
+    typealias ValidationPublisher = AnyPublisher<Validation, Never>
+    
+    // Validates whether a string property is non-empty.
+    static func nonEmptyValidation(for publisher: Published<String>.Publisher,
+                                   errorMessage: @autoclosure @escaping ValidationErrorClosure, dropFirst: Bool) -> ValidationPublisher {
+        return publisher.map { value in
+            guard value.count > 0 else {
+                return .failure(message: errorMessage())
+            }
+            return .success
+        }
+        .dropFirst(dropFirst ? 1 : 0) // If we are dealing with a non-empty form 
+        .eraseToAnyPublisher()
+    }
+    
+    #warning("TODO: Regex validation for email")
+    static func regExValidation() {
         
     }
 }
