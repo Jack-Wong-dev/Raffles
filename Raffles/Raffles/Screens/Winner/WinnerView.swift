3088759d05573843f5ec90796c5c2c1c4752dac4
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 23:08:51 -0400

    Created WinnerView and WinnerViewModel files.

---
+++ Raffles/Raffles/Screens/Winner/WinnerView.swift
@@ -0,0 +1,20 @@
+//
+//  WinnerView.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/30/21.
+//
+
+import SwiftUI
+
+struct WinnerView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct WinnerView_Previews: PreviewProvider {
+    static var previews: some View {
+        WinnerView()
+    }
+}
---
+++ Raffles/Raffles/Screens/Winner/WinnerViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  WinnerViewModel.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/30/21.
+//
+
+import Foundation
+import Combine
+
+final class WinnerViewModel: ObservableObject {
+    init() {
+        
+    }
+}
