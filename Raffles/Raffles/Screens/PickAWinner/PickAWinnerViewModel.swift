de8d0498cb3b33892fa9cdde8ab44f29b1fd30c8
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 22:25:46 -0400

    Created PickWinnerResponse encodable Model.

--- Raffles/Raffles/Screens/PickAWinner/PickAWinnerViewModel.swift
+++ Raffles/Raffles/Screens/PickAWinner/PickAWinnerViewModel.swift
@@ -8,10 +8,44 @@
 import Foundation
 import Combine
 
+public struct PickWinnerResponse: Decodable {
+  let id: Int
+  let raffleId: Int
+  let firstname: String
+  let lastname: String
+  let email: String
+  let phone: String?
+  let registeredAt: Date
+
+  private enum CodingKeys: String, CodingKey {
+    case id
+    case raffleId = "raffle_id"
+    case firstname
+    case lastname
+    case email
+    case phone
+    case registeredAt = "registered_at"
+  }
+}
+
 final class PickAWinnerViewModel: ObservableObject {
     @Published var secretToken: String
     
     init(secretToken: String = "") {
         self.secretToken = secretToken
     }
+}
+
+//MARK: Network methods
+extension PickAWinnerViewModel {
+    typealias WinnerPublisher = AnyPublisher<PickWinnerResponse, APIError>
+    func pickWinner()  {
+        
+    }
+    
+//    private func winnerPublisher() -> WinnerPublisher {
+//        RaffleAPIClient
+//            .shared
+//
+//    }
 }
