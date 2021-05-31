ed50afb5c153b0a42b70c89e65857d7ed372b125
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-31 14:41:19 -0400

    Created ValidationPublishers class

---
+++ Raffles/Raffles/Utilities/Validation/ValidationPublishers.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationPublishers.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/31/21.
+//
+
+import Foundation
+import Combine
+
+final class ValidationPublishers {
+    init() {
+        
+    }
+}
+
