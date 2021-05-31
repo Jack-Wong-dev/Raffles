3b8e6aace9407192bae7c01508bae83356c42cd6
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 20:51:05 -0400

    Added LinearGradient to the RaffleParticipantsView.

--- Raffles/Raffles/Screens/RaffleParticipantsView/RaffleParticipantsView.swift
+++ Raffles/Raffles/Screens/RaffleParticipantsView/RaffleParticipantsView.swift
@@ -11,74 +11,69 @@ struct RaffleParticipantsView: View {
     @StateObject var viewModel: ParticipantsViewModel
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack(spacing: 20) {
-                Text("Raffle App")
-                    .font(.largeTitle)
-                    .bold()
-                    .frame(maxWidth: .infinity)
-                
-                Text("Sample Raffle")
-                    .font(.title)
-                    .bold()
-                    .frame(maxWidth: .infinity)
-                
-                HStack(spacing: 0) {
-                    Group {
-                        VStack {
-                            Image(systemName: "ticket.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 22)
-                            Text("All Raffles")
-                        }
-                        
-                        VStack {
-                            Image(systemName: "square.and.pencil")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 22)
-                            Text("Register")
-                        }
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.lighting, Color.background]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    HStack(spacing: 0) {
+                        Group {
+                            VStack {
+                                Image(systemName: "ticket.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 22)
+                                Text("All Raffles")
+                            }
+                            
+                            VStack {
+                                Image(systemName: "square.and.pencil")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 22)
+                                Text("Register")
+                            }
 
-                        VStack {
-                            Image(systemName: "person.3.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 22)
-                            Text("Participants")
-                                .lineLimit(1)
-                        }
+                            VStack {
+                                Image(systemName: "person.3.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 22)
+                                Text("Participants")
+                                    .lineLimit(1)
+                            }
 
-                        VStack {
-                            Image(systemName: "crown.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 22)
-                            Text("Pick Winner")
-                                .lineLimit(2)
+                            VStack {
+                                Image(systemName: "crown.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 22)
+                                Text("Pick Winner")
+                                    .lineLimit(2)
+                            }
                         }
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity)
                     }
-                    .font(.subheadline)
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                            .strokeBorder(style: StrokeStyle(lineWidth: 2))
+                    )
+                    
+                    Text("Participants: \(viewModel.allParticipants.count) total")
+                        .font(.title2)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    ParticipantsView(participants: viewModel.allParticipants)
+                    
+                    Spacer(minLength: 0)
                 }
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                        .strokeBorder(style: StrokeStyle(lineWidth: 2))
-                )
-                
-                Text("Participants: \(viewModel.allParticipants.count) total")
-                    .font(.title2)
-                    .fontWeight(.medium)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                ParticipantsView(participants: viewModel.allParticipants)
-                
-                Spacer(minLength: 0)
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         }
     }
 }
