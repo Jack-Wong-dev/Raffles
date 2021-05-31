b3c2485a3f980ca74e54337e9255fec2e7a84e15
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-31 11:34:42 -0400

    Added new color assets and their respective Color extensions.

--- Raffles/Raffles/Utilities/Extension/Color-Extension.swift
+++ Raffles/Raffles/Utilities/Extension/Color-Extension.swift
@@ -11,6 +11,8 @@ extension Color {
     static let background = Color("background")
     static let shadow = Color("shadow")
     static let lighting = Color("lighting")
+    static let gradientStart = Color("GradientStart")
+    static let gradientEnd = Color("GradientEnd")
     
     static func getSelectionColor(using screen: ScreenOption) -> Color {
         switch screen {
