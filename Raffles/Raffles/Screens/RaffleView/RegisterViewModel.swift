d1e32873f2bb10cd06429c84e3678a43e0696a2f
Jack Wong <jackwong@Jacks-Mac-mini.fios-router.home>
2021-05-30 21:05:10 -0400

    Created RegisterViewModel.

---
+++ Raffles/Raffles/Screens/RaffleView/RegisterViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterViewModel.swift
+//  Raffles
+//
+//  Created by Jack Wong on 5/30/21.
+//
+
+import Foundation
+import Combine
+
+final class RegisterViewModel: ObservableObject {
+    init() {}
+}
